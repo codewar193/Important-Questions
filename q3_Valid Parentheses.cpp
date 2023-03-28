cd90e01091bd7c8466885acdc382d896f147e6b3
S. Manna <90498811+snehamanna559@users.noreply.github.com>
2023-03-28 17:59:28 +0530

    Create q3_Valid Parentheses.cpp

---
+++ q3_Valid Parentheses.cpp
@@ -0,0 +1,67 @@
+/*Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+Constraints:
+    1 <= s.length <= 104
+    s consists of parentheses only '()[]{}'.*/
+
+
+#include<iostream>
+#include<stack>
+using namespace std;
+bool isBalanced(string expr) {
+   stack<char> s;
+   char ch;
+   for (int i=0; i<expr.length(); i++) {    //for each character in the expression, check conditions
+      if (expr[i]=='('||expr[i]=='['||expr[i]=='{') {    //when it is opening bracket, push into     stack
+         s.push(expr[i]);
+      }
+      if (s.empty())    //stack cannot be empty as it is not opening bracket, there must be closing     bracket
+         return false;
+         switch (expr[i]) {
+            case ')':    //for closing parenthesis, pop it and check for braces and square brackets
+               ch = s.top();
+               s.pop();
+               if (ch=='{' || ch=='[')
+                  return false;
+                  break;
+            case '}': //for closing braces, pop it and check for parenthesis and square brackets
+               ch = s.top();
+               s.pop();
+               if (ch=='(' || ch=='[')
+                  return false;
+                  break;
+            case ']': //for closing square bracket, pop it and check for braces and parenthesis
+               ch = s.top();
+               s.pop();
+               if (ch =='(' || ch == '{')
+                  return false;
+                  break;
+         }
+      }
+      return (s.empty()); //when stack is empty, return true
+}
+main() {
+   string expr = "[{}(){()}]";
+   if (isBalanced(expr))
+      cout << "Balanced";
+   else
+      cout << "Not Balanced";
+}
+Output
