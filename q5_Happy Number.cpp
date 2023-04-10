a2998fc9bacfd15bf99d82237ff3ff3aae8cc9d3
S. Manna <90498811+snehamanna559@users.noreply.github.com>
2023-04-10 13:19:22 +0530

    Update q5_Happy Number.cpp

--- q5_Happy Number.cpp
+++ q5_Happy Number.cpp
@@ -1,4 +1,17 @@
+/*
+Given n ropes of different lengths, connect them into a single rope with minimum cost. Assume that the cost to connect two ropes is the same as the sum of their lengths.
 
+For example,
+
+Input:  [5, 4, 2, 8]
+ 
+Output: The minimum cost is 36
+  
+[5, 4, 2, 8] –> First, connect ropes of lengths 4 and 2 that will cost 6.
+[5, 6, 8]    –> Next, connect ropes of lengths 5 and 6 that will cost 11.
+[11, 8]      –> Finally, connect the remaining two ropes that will cost 19.
+ 
+Therefore, the total cost for connecting all ropes is 6 + 11 + 19 = 36.*/
 // Time complexity: O(log n) for a given number n
 
 // Auxiliary Space: O(n)
