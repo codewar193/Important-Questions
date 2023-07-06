2a2ce5d835c9d73ac92fcddb0e5d8903ebcf0d09
S. Manna <90498811+codewar193@users.noreply.github.com>
2023-07-07 00:29:43 +0530

    easy#geeksforgeels

---
+++ q10_Thief trying to escape.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int totalJumps(int X, int Y, int N, int arr[]){
+        // code here
+        
+        int count =0;
+        
+        for(int i =0;i<N;i++)
+        {
+            
+                count = count+1;
+            
+                while(arr[i]>X)
+                {
+                    arr[i] = arr[i] -(X-Y);
+                    count++;
+                }
+            
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int X, Y, N;
+        cin>>X>>Y>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        cout<<ob.totalJumps(X, Y, N, arr)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
