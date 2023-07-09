a7cfbf7aed6c69462feaf385e87815a52a7bb05b
S. Manna <90498811+codewar193@users.noreply.github.com>
2023-07-09 12:40:56 +0530

    eas

---
+++ q11_Segregate Even and Odd numbers.cpp
@@ -0,0 +1,21 @@
+  class Solution {
+public:
+    void segregateEvenOdd(int arr[], int n) {
+        
+        
+     sort(arr,arr+n);
+     int eve=0;
+
+    for(int i=0;i<n;i++){
+     if(arr[i]%2==0)
+    {
+      swap(arr[eve],arr[i]);
+       eve++;
+     }  
+    
+       
+   }
+   sort(arr+eve,arr+n);
+}
+    
+};
