2617f31eb846cf12dc626b701fa4906cdc35a71f
S. Manna <90498811+codewar193@users.noreply.github.com>
2023-07-10 21:19:25 +0530

    medium #leetcode

--- q13_Kth Largest Element in an Array.cpp
+++ q13_Kth Largest Element in an Array.cpp
@@ -1,8 +1,25 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size()-1;
-        sort(nums.begin(),nums.end());
-        return nums[n-k+1];
+
+  //Method-1
+    // int findKthLargest(vector<int>& nums, int k) {
+    //     int n = nums.size()-1;
+    //     sort(nums.begin(),nums.end());
+    //     return nums[n-k+1];
+    // }
+
+ //Method-2
+     int findKthLargest(vector<int>& nums, int k) {
+
+        priority_queue<int>pq(nums.begin(),nums.end());
+        int ans;
+
+        while(k--)
+        {
+            ans = pq.top();
+            pq.pop();
+        }
+      return ans;
+
     }
 };
