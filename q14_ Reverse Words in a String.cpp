709a37b6c6d24d3ad8f1d3174e70b76fee5c152d
S. Manna <90498811+codewar193@users.noreply.github.com>
2023-07-12 01:37:02 +0530

    medium#leetcode

--- q14_ Reverse Words in a String.cpp
+++ q14_ Reverse Words in a String.cpp
@@ -1,1 +1,34 @@
-jjj
+//method-1 TC-O(n) SC-O(1)
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+     int space = 0;
+     string result = "";
+     int n = s.length();
+   
+     while(space<n)
+     {
+       while(space<n && s[space]==' ')
+       {
+           space++;
+       }
+       if(space>=n)break;
+
+        int  skipspace = space + 1;
+
+        while(skipspace<n && s[skipspace]!=' ')
+       {
+           skipspace++;
+       }
+
+       string sub = s.substr(space,skipspace-space);
+       if(result.length() == 0) result = sub;
+       else result = sub + " " + result;
+
+       space = skipspace+1;
+     }
+     return result;
+    }
+};
